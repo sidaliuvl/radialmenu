127ade47f0705c5bca6a8f1e2497d9c21e154d3f
sidaliuvl <111689430+sidaliuvl@users.noreply.github.com>
2025-05-21 08:46:39 +0100

    Complete Radial Menu Redesign

--- radialmenu/main.js
+++ radialmenu/main.js
@@ -7,6 +7,13 @@ $(function() {
         var item = event.data;
         if (item.type == "show") {
             $('body').fadeIn(300);
+            $('.circleArea').css('transform', 'scale(0.9)').animate({
+                opacity: 1,
+                scale: '1'
+            }, {
+                duration: 300,
+                easing: 'easeOutCubic'
+            });
         }
         if (item.type == "close") {
             exit();
@@ -23,7 +30,11 @@ $(function() {
             currentCombination = 2;
 
             var totalPages = Math.ceil(item.number / itemsPerPage);
-            $('.next-page').toggle(totalPages > 1);
+            if (totalPages > 1) {
+                $('.next-page').fadeIn(300);
+            } else {
+                $('.next-page').hide();
+            }
             $('.back-page').hide();
         }
         if (item.type == "add") {
@@ -33,33 +44,76 @@ $(function() {
 });
 
 function addItem(item) {
-    let div = `<div onclick="selectBox('${item.id}')" class="Box circle-${currentCombination} delete page-${Math.ceil(item.number / itemsPerPage)}">
-        <div class="boxBg"></div>
-        <i class="fa-solid ${item.data.icon} icon"></i>
-        <div class="boxText">${item.data.label}</div>
-    </div>`;
+    let div = `
+        <div onclick="selectBox('${item.id}')" class="Box circle-${currentCombination} delete page-${Math.ceil(item.number / itemsPerPage)}">
+            <i class="fa-solid ${item.data.icon} icon"></i>
+            <div class="boxText">${item.data.label}</div>
+        </div>
+    `;
     
     $('.circleArea').append(div);
 
-    let exitDiv = `<div onclick="exit()" class="Box ex delete">
-        <div class="boxBg exitBox"></div>
-        <i class="fa-solid fa-xmark icon"></i>
-        <div class="boxText">EXIT</div>
-    </div>`;
+    let exitDiv = `
+        <div onclick="exit()" class="Box ex delete">
+            <i class="fa-solid fa-xmark icon"></i>
+            <div class="boxText">EXIT</div>
+        </div>
+    `;
     
     $('.circleArea').append(exitDiv);
 
+    // Add navigation buttons
+    let navButtons = `
+        <div class="next-page delete">
+            <i class="fa-solid fa-chevron-right icon"></i>
+        </div>
+        <div class="back-page delete">
+            <i class="fa-solid fa-chevron-left icon"></i>
+        </div>
+    `;
+    
+    $('body').append(navButtons);
+
+    // Initialize page display
     showPage(currentPage, true);
+
+    // Animate items appearance
+    $('.Box').each(function(index) {
+        $(this).css({
+            'opacity': '0',
+            'transform': 'scale(0.5)'
+        }).delay(index * 50).animate({
+            opacity: 1,
+            scale: '1'
+        }, {
+            duration: 300,
+            easing: 'easeOutCubic'
+        });
+    });
 }
 
 function showPage(page, isFirst) {
     $('.circle-1, .circle-2').hide();
-    $('.page-' + page).show();
+    $(`.page-${page}`).show().css('display', 'flex');
 
     $('.next-page').toggle($('.page-' + (page + 1)).length > 0);
     if (!isFirst) {
         $('.back-page').toggle(page > 1);
     }
+
+    // Animate page transition
+    $('.Box:not(.ex)').each(function(index) {
+        $(this).css({
+            'opacity': '0',
+            'transform': 'scale(0.5)'
+        }).delay(index * 50).animate({
+            opacity: 1,
+            scale: '1'
+        }, {
+            duration: 300,
+            easing: 'easeOutCubic'
+        });
+    });
 }
 
 function selectBox(id) {
@@ -67,10 +121,30 @@ function selectBox(id) {
 }
 
 function exit() {
-    $('body').fadeOut(300, function() {
-        $.post('https://qb-radialmenu/exit', JSON.stringify({}));
+    $('.circleArea').animate({
+        opacity: 0,
+        scale: '0.9'
+    }, {
+        duration: 300,
+        easing: 'easeInCubic',
+        complete: function() {
+            $('body').fadeOut(300, function() {
+                $.post('https://qb-radialmenu/exit', JSON.stringify({}));
+            });
+        }
     });
 }
+
+// Navigation handlers
+$(document).on('click', '.next-page', function() {
+    currentPage++;
+    showPage(currentPage, false);
+});
+
+$(document).on('click', '.back-page', function() {
+    currentPage--;
+    showPage(currentPage, false);
+});
 
 $(document).keyup(function(e) {
     if (e.key === "Escape") {
